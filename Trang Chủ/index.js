d48e9a258ccdd5ea6c8ba2357ed3aa5b762694a0
phantu18 <120042273+phantu18@users.noreply.github.com>
2023-10-25 18:04:19 +0700

    project

--- Trang Chủ/index.js
+++ Trang Chủ/index.js
@@ -1,37 +1,13 @@
-// let list = document.querySelector(".slider .list");
-// let items = document.querySelectorAll(".slider .list .item");
-// let dots = document.querySelectorAll(".slider .dots li");
-// let prev = document.getElementById("prev");
-// let next = document.getElementById("next");
+// Menu Topp
+const header = document.querySelector(".header");
+const headerTop = header.getBoundingClientRect().top;
 
-// let active = 0;
-// let lengthItems = items.length - 1;
-// next.onclick = function () {
-//   if (active + 1 > lengthItems) {
-//     active = 0;
-//   } else {
-//     active = active + 1;
-//   }
-//   reloadSlider();
-// };
-// function reloadSlider() {
-//   let checkleft = items[active].offsetLeft;
-//   list.computedStyleMap.left = -checkleft + "px";
-//   let las;
-// }
+document.body.style.setProperty("--_header-h", `${header.offsetHeight}px`);
 
-// B   A   N   N   E   R   4   ---   S   L   I   D   E
-function slide_in() {
-  var reveals = document.getElementsByClassName('slideIn');
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    if (elementTop < windowHeight - 200) {
-      reveals[i].classList.add("active");
-    } else if ( elementTop > windowHeight - 200) {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
-window.addEventListener("scroll", slide_in);
+window.addEventListener("scroll", function () {
+  header.classList.toggle(
+    "is-sticky",
+    window.scrollY > headerTop + header.offsetHeight
+  );
+});
+// End menu
